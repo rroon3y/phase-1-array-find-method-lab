4fb75bd1bfec0c8c0dc212407bbc9f0f4f33663f
Rroon3y <rooneyalex2016@gmail.com>
2025-06-11 09:08:55 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(entry => entry.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+module.exports = superbowlWin;
